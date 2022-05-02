afc2e8fa6e170e80563fd673108955977f4cfbd8
Ana Helena <anahelenarp@hotmail.com>
2022-05-02 19:37:18 -0300

    chore: add tailwindcss; add button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
-function App() {
-  return <h1>Hello World</h1>
+import { Widget } from "./components/Widget"
+
+type ButtonProps = {
+  text: string
 }
 
-export default App
+function Button (props: ButtonProps) {
+  return <button className="bg-purple-700 p-2 h-10 rounded px-4 hover:bg-violet-100">{props.text}</button>
+}
+
+export function App() {
+  return <Widget />
+}
